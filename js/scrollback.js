9cdc1377d85d793b24545d9bd6ae6ac93f110d46
Andrew Cook <thecourtjester@twelvebaud.net>
2012-06-13 16:15:17 -0400

    [FAQ] scrollingPrecision++, added topic stubs

--- js/scrollback.js
+++ js/scrollback.js
@@ -1,7 +1,8 @@
-﻿window.onhashchange = function(e) {
+﻿var hashevt = function(e) {
         if(location.hash.length < 2)
                 return;
-        if($(window).scrollTop() + 110 > $(location.hash).offset().top)
-                window.scrollBy(0, -110);
+        window.scrollTo(0, $(location.hash).offset().top - 110);
         $(location.hash).css("color", "#2f96b4").delay(2000).animate({"color" : "#333333"}, 2000);
 }
+window.onhashchange = hashevt;
+window.setTimeout(hashevt, 333);
